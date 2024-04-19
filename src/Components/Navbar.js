ae179abb11247b857ee06942e2f72392284ec6d8
Nikhil Kharche <140874912+NikhilKharche1011@users.noreply.github.com>
2024-04-19 12:14:04 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Flex, Text } from '@chakra-ui/react'
+import { MdOutlineLightMode } from "react-icons/md";
+import { HiMenuAlt3 } from "react-icons/hi";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Flex w='100%' h='100%'>
+      <Flex py='0.5%' px='10%' w='100%' justifyContent='space-between'>
+        <Flex>Nikhil</Flex>
+        <Flex>
+          <Flex mx='10px' fontSize='25px'>
+            <MdOutlineLightMode />
+          </Flex>
+          <Flex mx='10px' fontSize='25px'>
+            <HiMenuAlt3 />
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
   )
 }
 
